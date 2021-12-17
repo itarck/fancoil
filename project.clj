cc3eb520ead0720250249a4cb4d4b10bfda3bc08
itarck <itarck@gmail.com>
2021-12-17 14:07:38 +0800

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject com.github.itarck/fancoil "0.0.1-SNAPSHOT"
+  :description "A clojurescript framework, which uses multi-methods to define and implement system unit, use integrant to inject configuration and stateful dependencies to unit at system startup."
+  :url "https://github.com/itarck/fancoil"
+  :license {:name "MIT License"
+            :url  "https://opensource.org/licenses/MIT"}
+  :source-paths ["src/main"])
