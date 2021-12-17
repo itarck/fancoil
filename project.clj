67399532950f3619648e9078cfb3df65103ec0ec
itarck <itarck@gmail.com>
2021-12-17 16:25:46 +0800

    Release 0.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject com.github.itarck/fancoil "0.0.1-SNAPSHOT"
-  :description "A clojurescript framework, which uses multi-methods to define and implement system unit, use integrant to inject configuration and stateful dependencies to unit at system startup."
+(defproject com.github.itarck/fancoil "0.0.2-SNAPSHOT"
+  :description "A clojurescript framework, which uses multi-methods to define and implement system unit, uses integrant to inject configuration and stateful dependencies to unit at system startup."
   :url "https://github.com/itarck/fancoil"
   :license {:name "MIT License"
             :url  "https://opensource.org/licenses/MIT"}
