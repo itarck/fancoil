7ddd2e04da91101a013616996fa67886594078f5
itarck <itarck@gmail.com>
2022-03-31 11:44:43 +0800

    Refactor to units and system

---
+++ src/main/fancoil/system.cljs
@@ -0,0 +1,156 @@
+(ns fancoil.system
+  (:require
+   [cljs.core.async :refer [go go-loop >! <! chan]]
+   [integrant.core :as ig]
+   [reagent.dom :as rdom]
+   [reagent.core :as r]
+
+   [fancoil.units :as units]
+
+;;    [fancoil.modules.datascript]
+;;    [fancoil.modules.cljs-ajax]
+;;    [fancoil.modules.html-router]
+   ))
+
+;; helper function
+
+(defmethod ig/init-key ::value
+  [_ config]
+  config)
+
+(defmethod ig/init-key ::spec
+  [_ config]
+  (units/create-spec-instance config))
+
+(defmethod ig/init-key ::ratom
+  [_ config]
+  (units/create-ratom config))
+
+(defmethod ig/init-key ::pconn
+  [_k config]
+  (units/create-poshed-datascript-conn config))
+
+(defmethod ig/init-key ::schema
+  [_ config]
+  (units/create-schema-instance config))
+
+(defmethod ig/init-key ::subscribe
+  [_ config]
+  (units/create-subscribe-instance config))
+
+(defmethod ig/init-key ::view
+  [_ config]
+  (units/create-view-instance config))
+
+(defmethod ig/init-key ::model
+  [_ config]
+  (units/create-model-instance config))
+
+(defmethod ig/init-key ::handle
+  [_ config]
+  (units/create-handle-instance config))
+
+(defmethod ig/init-key ::schedule
+  [_ config]
+  (units/create-schedule-instance config))
+
+(defmethod ig/init-key ::chan
+  [_ _]
+  (chan))
+
+(defmethod ig/init-key ::dispatch
+  [_ config]
+  (units/create-dispatch-instance config))
+
+(defmethod ig/init-key ::inject
+  [_ config]
+  (units/create-inject-instance config))
+
+(defmethod ig/init-key ::do!
+  [_ config]
+  (units/create-do-instance config))
+
+(defmethod ig/init-key ::process
+  [_ config]
+  (units/create-process-instance config))
+
+(defmethod ig/init-key ::service
+  [_ config]
+  (units/create-service-instance config))
+
+;; ------------------------------------------------
+;; helper functions
+  
+(defn load-hierarchy
+  [hierarchy]
+  (doseq [[tag parents] hierarchy
+          parent parents]
+    (derive tag parent)))
+
+(defn merge-config
+  [old-config new-config]
+  (merge-with merge old-config new-config))
+
+
+;; ------------------------------------------------
+;; system
+
+(defmulti system-base
+  (fn [config method & args] method))
+
+(defmethod system-base :reload
+  [{:keys [state-atom config]} _ _]
+  (let [instance (ig/init config)]
+    (reset! state-atom instance)))
+
+(defmethod system-base :get-state
+  [core _ _]
+  (let [instance @(:state-atom core)]
+    instance))
+
+(defmethod system-base :get-unit
+  [core _ {:keys [unit-name]}]
+  (let [instance (system-base core :get-state {})]
+    (unit-name instance)))
+
+(defmethod system-base :mount-view
+  [core _ [view-method view-ctx] app-id]
+  (let [viewer (system-base core :get-unit {:unit-name ::view})]
+    (rdom/render
+     [viewer view-method view-ctx]
+     (.getElementById js/document app-id))))
+
+(defmethod system-base :dispatch
+  [core _ [method request]]
+  (let [dispatch-fn (system-base core :get-unit {:unit-name ::dispatch})]
+    (dispatch-fn method request)))
+
+(def default-config
+  {::schema {}
+   ::spec {}
+   ::pconn {:schema (ig/ref ::schema)
+            :initial-tx []}
+   ::subscribe {:pconn (ig/ref ::pconn)}
+   ::inject {:pconn (ig/ref ::pconn)
+             :inject-keys [:posh-db]}
+   ::do! {:pconn (ig/ref ::pconn)}
+   ::model {}
+   ::handle {:schema (ig/ref ::schema)
+             :model (ig/ref ::model)}
+   ::process {:inject (ig/ref ::inject)
+              :do! (ig/ref ::do!)
+              :handle (ig/ref ::handle)}
+   ::view {:schema (ig/ref ::schema)
+           :dispatch (ig/ref ::dispatch)
+           :subscribe (ig/ref ::subscribe)}
+   ::chan {}
+   ::dispatch {:out-chan (ig/ref ::chan)}
+   ::service {:process (ig/ref ::process)
+              :in-chan (ig/ref ::chan)}})
+
+(defn create-system-instance
+  [config]
+  (let [core {:state-atom (r/atom (ig/init config))
+              :config config}]
+    (fn [method & args]
+      (apply system-base core method args))))
---
+++ src/main/fancoil/units.cljs
@@ -0,0 +1,363 @@
+(ns fancoil.units
+  (:require
+   [cljs.core.async :refer [go go-loop >! <! chan]]
+   [cljs.spec.alpha :as s]
+   [datascript.core :as d]
+   [posh.reagent :as p]
+   [medley.core :as m]
+   [reagent.core :as r]
+   [reagent.ratom :as ra]))
+
+
+;; ctx spec
+
+(s/def ::method keyword?)
+(s/def ::request map?)
+(s/def ::action (s/cat :method ::method
+                       :request ::request))
+(s/def ::event map?)
+(s/def ::env map?)
+
+(s/def ::effect (s/coll-of (s/cat :method ::method
+                                  :fx some?)))
+(s/def ::scope map?)
+(s/def ::signal map?)
+(s/def ::hiccup vector?)
+(s/def ::props map?)
+
+
+;; ------------------------------------------------
+;; spec 
+
+(defmulti spec-base
+  (fn [core method & args] method))
+
+(defmethod spec-base :assert
+  [_ _ spec data]
+  (s/assert spec data))
+
+(defmethod spec-base :valid?
+  [_ _ spec data]
+  (s/valid? spec data))
+
+(defn create-spec-instance
+  [config]
+  (let [ms (methods spec-base)]
+    (doseq [[k f] ms]
+      (when (and
+             (qualified-keyword? k)
+             (not= (namespace k) "spec"))
+        (f)))
+    (s/check-asserts true)
+    (partial spec-base {})))
+
+;; ------------------------------------------------
+;; ratom
+
+(defn create-ratom
+  [config]
+  (let [{:keys [initial-value]} config]
+    (r/atom initial-value)))
+
+;; ------------------------------------------------
+;; poshed-ds
+
+(defn create-poshed-datascript-conn
+  [config]
+  (let [{:keys [schema initial-tx initial-db] :or {schema {}}} config
+        conn (d/create-conn schema)]
+    (when initial-db
+      (d/reset-conn! conn initial-db))
+    (when initial-tx
+      (d/transact! conn initial-tx))
+    (p/posh! conn)
+    conn))
+
+;; ------------------------------------------------
+;; datascript schema
+
+(defmulti schema-base
+  (fn [config signal & args] signal))
+
+(defn create-schema-instance
+  [config]
+  (let [schema-ref (atom {})]
+    (doseq [[k f] (methods schema-base)]
+      (let [schm (f)]
+        (swap! schema-ref merge schm)))
+    @schema-ref))
+
+;; ------------------------------------------------
+;; subscribe
+
+(defmulti subscribe-base
+  "stateful function: subscribe a ratom or reaction
+   reaction or ratom in -> reaction out
+   core: db-ref
+   "
+  (fn [core method & args] method))
+
+(defmethod subscribe-base :pull
+  [{:keys [pconn]} _ {:keys [selector id]}]
+  (p/pull pconn selector id))
+
+(defmethod subscribe-base :q
+  [{:keys [pconn]} _ {:keys [query inputs]}]
+  (apply p/q query (concat [pconn] inputs)))
+
+(s/def ::subscribe.config map?)
+(s/def ::subscribe.input map?)
+(s/def ::subscribe.output (fn [t] (= ra/Reaction (type t))))
+
+(defn create-subscribe-instance
+  [config]
+  (fn [method signal]
+    (let [core config]
+      (try
+        (s/assert ::subscribe.input signal)
+        (let [output (subscribe-base core method signal)]
+          (s/assert ::subscribe.output output)
+          output)
+        (catch js/Object e (println "error in system/subscribe: " (str e)))))))
+
+;; ------------------------------------------------
+;; view
+
+(defmulti view-base
+  "stateful function: view a entity
+   props in -> reagent views
+   core: subscribe, dispatch
+   "
+  (fn [core method & args] method))
+
+
+(s/def ::view.config map?)
+(s/def ::view.method ::method)
+(s/def ::view.input ::scope)
+(s/def ::view.output some?)
+
+(defn create-view-instance
+  [config]
+  (fn [method scope]
+    (let [core config]
+      (try
+        (s/assert ::view.input scope)
+        (let [output (view-base core method scope)]
+          (s/assert ::view.output output)
+          output)
+        (catch js/Object e (println "error in system/view: " (str e)))))))
+
+
+;; ------------------------------------------------
+;; dispatch 
+
+(s/def ::dispatch.input ::request)
+
+(defn create-dispatch-instance
+  [config]
+  (let [{:keys [out-chan]} config]
+    (fn [method request]
+      (go (>! out-chan [method request])))))
+
+
+;; ------------------------------------------------
+;; inject
+
+(defmulti inject-base
+  "stateful function: inject a cofx
+   request in -> request out
+   core: db-ref, other resources
+   "
+  (fn [core method & args] method))
+
+(defmethod inject-base :inject-all
+  [{:keys [inject-keys] :as core} _ request]
+  (reduce (fn [req k]
+            (inject-base core k req))
+          request inject-keys))
+
+(defmethod inject-base :posh-db
+  [{:keys [pconn]} _method req]
+  (assoc-in req [:env :posh-db] @pconn))
+
+(defmethod inject-base :ratom-db
+  [{:keys [ratom]} _method req]
+  (assoc-in req [:env :ratom-db] @ratom))
+
+(s/def ::inject.input (s/keys :req-un [::event] :opt-un [::env]))
+(s/def ::inject.output (s/keys :req-un [::event ::env]))
+
+(defn create-inject-instance
+  [config]
+  (fn [method req]
+    (let [core config]
+      (try
+        (s/assert ::inject.input req)
+        (let [output (inject-base core method req)]
+          (s/assert ::inject.output output)
+          output)
+        (catch js/Object e (println (str e)))))))
+
+;; ------------------------------------------------
+;; model
+
+(defmulti model-base
+  "pure function: tap a model
+   value in -> value out
+   "
+  (fn [core method & args] method))
+
+(defn create-model-instance
+  [config]
+  (partial model-base config))
+
+;; ------------------------------------------------
+;; handle
+
+(defmulti handle-base
+  "pure function: handle a request
+   request in -> response out
+   {:db db} -> {:tx tx}"
+  (fn [core method & args] method))
+
+(s/def ::handle.input (s/keys :req-un [::event ::env]))
+(s/def ::handle.output ::effect)
+
+(defn create-handle-instance
+  [config]
+  (fn [method req]
+    (let [core config]
+      (try
+        (s/assert ::handle.input req)
+        (let [output (handle-base core method req)]
+          (s/assert ::handle.output output)
+          output)
+        (catch js/Object e (println "error in system/handle: " (str e)
+                                    "method: " method
+                                    "request: " (str req)))))))
+
+;; ------------------------------------------------
+;; do 
+
+(defmulti do-base
+  "stateful function: do a fx
+   response in -> do effects
+   core: db-ref, other resources
+   "
+  (fn [core method & args] method))
+
+(defmethod do-base :dispatch
+  [{:keys [dispatch]} _ action]
+  (let [[method request] action]
+    (dispatch method request)))
+
+(defmethod do-base :dispatch-many
+  [{:keys [dispatch]} _ actions]
+  (doseq [[method request] actions]
+    (dispatch method request)))
+
+(defmethod do-base :log-out
+  [_ _ value]
+  (println "do.log/out: " value))
+
+(defmethod do-base :log-error
+  [_ _ value]
+  (println "do.log/error: " value))
+
+(defmethod do-base :tx
+  [{:keys [pconn]} _ tx]
+  (p/transact! pconn tx)
+  tx)
+
+(defmethod do-base :reset-ratom
+  [{:keys [ratom]} _ db-value]
+  (reset! ratom db-value))
+
+(defmethod do-base :set-ratom-paths
+  [{:keys [ratom]} _ path-value-pairs]
+  (doseq [[path value] path-value-pairs]
+    (swap! ratom assoc-in path value)))
+
+(defmethod do-base :delete-ratom-paths
+  [{:keys [ratom]} _ paths]
+  (doseq [path paths]
+    (swap! ratom m/dissoc-in path)))
+
+(defmethod do-base :stdout
+  [{:keys [stdout-chan]} _ data]
+  (go (>! stdout-chan data)))
+
+
+(defn create-do-instance
+  [config]
+  (fn [method req]
+    (let [core config]
+      (do-base core method req))))
+
+
+;; ------------------------------------------------
+;; process
+
+(defmulti process-base
+  "stateful function: process a request to fx
+   request in -> effects
+   core: ratom, other resources
+   "
+  (fn [core method & args] method))
+
+(defmethod process-base :default
+  [{:keys [do! handle inject] :as core} method req]
+  (if (qualified-keyword? method)
+    (let [req (inject :inject-all req)
+          effect (handle method req)]
+      (doseq [[k v] effect]
+        (if (qualified-keyword? k)
+          (process-base core k v)
+          (do! k v))))
+    (do! method req)))
+
+(s/def ::process.input (s/keys :req-un [::event]
+                               :opt-un [::env]))
+
+(defn create-process-instance 
+  [config]
+  (fn [method req]
+    (let [core config]
+      (try
+        (s/assert ::do.input req)
+        (let [output (process-base core method req)]
+          output)
+        (catch js/Object e (println "error in system/process: " (str e)
+                                    "method: " method
+                                    "request: " (str req))))))
+  (partial process-base config))
+
+
+;; ------------------------------------------------
+;; service
+
+(defn create-service-instance
+  [config]
+  (let [{:keys [process in-chan]} config]
+    (go-loop []
+      (let [[method request] (<! in-chan)]
+        (if (get-in request [:props :sync?])
+          (process method request)
+          (go (process method request))))
+      (recur))
+    {:in-chan in-chan}))
+
+
+;; ------------------------------------------------
+;; schedule
+
+(defmulti schedule-base
+  "stateful function: schedule a task once or periodic
+   task in -> event out
+   core: dispatch
+   "
+  (fn [core method & args] method))
+
+(defn create-schedule-instance
+  [config]
+  (partial schedule-base config))
